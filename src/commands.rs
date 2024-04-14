57924afbd48d5c6812d8e0c67a4389b39a753549
Chaaruhaas Kandregula <chaaskandregula@gmail.com>
2024-04-13 20:33:11 -0700

    made changes to how the placement is given, will start accepting fields from discord instead of manually hardcoding it in

--- src/commands.rs
+++ src/commands.rs
@@ -18,20 +18,31 @@ pub async fn help(ctx: Context<'_>, command: Option<String>) -> Result<(), Error
 #[poise::command(prefix_command, track_edits)]
 // this is the rank query function, taking in 4 arguments: year, invy, school, event
 pub async fn rq(ctx: Context<'_>, _command: Option<String>) -> Result<(), Error> {
-    poise::say_reply(ctx, "oof").await?;
+    let year = 2024;
+    let invy = String::from("states");
+    let event = String::from("Chemistry Lab");
+    let school = String::from("Lynbrook");
+    let query =
+        parse_file::Query::build_query(year.clone(), invy.clone(), school.clone(), event.clone());
+    let x = query.find_rank().to_string();
+    let mut out_string = String::new();
+    out_string.push_str(&school);
+    out_string.push_str(" ");
+    out_string.push_str(&event);
+    out_string.push_str("'s placement at ");
+    out_string.push_str(&invy);
+    out_string.push_str(" ");
+    out_string.push_str(&year.to_string());
+    out_string.push_str(" is: ");
+    out_string.push_str(&x);
+    query.print_fields();
+    println!("{x}");
+    poise::say_reply(ctx, out_string).await?;
     Ok(())
 }
 
 #[poise::command(prefix_command, track_edits)]
 pub async fn chat(ctx: Context<'_>, _command: Option<String>) -> Result<(), Error> {
     poise::say_reply(ctx, "chat it might be over :(").await?;
-    let year = 2024;
-    let invy = String::from("states");
-    let event = String::from("Chemistry Lab");
-    let school = String::from("Lynbrook");
-    let query = parse_file::Query::build_query(year, invy, school, event);
-    let x = query.find_rank();
-    query.print_fields();
-    println!("{x}");
     Ok(())
 }
